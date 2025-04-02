55abe3723c7910f790bb3e4dd1bc9f3761ee2743
Majo <mvargashurtad031@gmail.com>
2025-04-02 10:33:31 -0500

    todos los archivos fueron subido gracias a dios

---
+++ cmake-build-debug/CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/git/contenedores/9-practica-contenedores-toreno-Majito167/main.cpp" "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/main.cpp.obj" "gcc" "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/main.cpp.obj.d"
+  "C:/git/contenedores/9-practica-contenedores-toreno-Majito167/src/Jugador.cpp" "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Jugador.cpp.obj" "gcc" "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Jugador.cpp.obj.d"
+  "C:/git/contenedores/9-practica-contenedores-toreno-Majito167/src/Torneo.cpp" "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Torneo.cpp.obj" "gcc" "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Torneo.cpp.obj.d"
+  "C:/git/contenedores/9-practica-contenedores-toreno-Majito167/src/Videojuego.cpp" "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Videojuego.cpp.obj" "gcc" "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Videojuego.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "9_practica_contenedores_torneo_Majito167.exe"
+  "9_practica_contenedores_torneo_Majito167.exe.manifest"
+  "9_practica_contenedores_torneo_Majito167.pdb"
+  "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/main.cpp.obj"
+  "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/main.cpp.obj.d"
+  "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Jugador.cpp.obj"
+  "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Jugador.cpp.obj.d"
+  "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Torneo.cpp.obj"
+  "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Torneo.cpp.obj.d"
+  "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Videojuego.cpp.obj"
+  "CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/src/Videojuego.cpp.obj.d"
+  "lib9_practica_contenedores_torneo_Majito167.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9_practica_contenedores_torneo_Majito167.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
